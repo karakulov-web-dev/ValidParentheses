da206cf1f13e5fb286387f3920fa0ee4a743b3d6
karakulov.web.dev@gmail.com <karakulov.web.dev@gmail.com>
2023-06-03 03:43:58 +0300

    add solution

--- ValidParentheses.py
+++ ValidParentheses.py
@@ -1,1 +1,14 @@
 
+class Solution:
+    def isValid(self, s) -> bool:
+        stack = []
+        pairs = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            if char in pairs.values():
+                stack.append(char)
+            elif char in pairs.keys():
+                if not stack or stack.pop() != pairs[char]:
+                    return False
+        
+        return len(stack) == 0
